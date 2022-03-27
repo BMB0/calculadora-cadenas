81bc48d3705bc9a6a8fdf837e41df672ffb07a89
BMB <bruno.marquez@ucb.edu.bo>
2022-03-27 15:39:00 -0400

    Se soporta cadenas separadas con delimitadores especificados

--- src/calculador_cadenas.js
+++ src/calculador_cadenas.js
@@ -7,14 +7,34 @@ function calculador(cadena) {
   }
   else if(cadena.search(",") != -1 || cadena.search("-") != -1) 
   {
-    const cadenaArray = cadena.split(/[,-]+/);
+    res = splitAdd(cadena, "");
+  } else if(cadena[0 ]=== "/")
+  {
+    res = get_separator(cadena);
+  }else res = parseInt(cadena);
+
+  return res;
+}
+function get_separator(cadena)
+{
+  var cadenaArray;
+  cadenaArray = cadena.split(/[[\n ]+/);
+  return splitAdd(cadenaArray[2], cadenaArray[1].slice(0,-1));
+}
+function splitAdd(cadena, separador)
+{
+  var res = 0;
+  var cadenaArray;
+  if(separador === "")
+  {
+    cadenaArray = cadena.split(/[,-]+/);
+  }else cadenaArray = cadena.replaceAll(separador, ',').split(/[,-]+/);
+  
     for(var i = 0; i < cadenaArray.length ; i++)
     {
       res = res + parseInt(cadenaArray[i]);
     }
-  } else res = parseInt(cadena);
-
-  return res;
+    return res;
 }
 
 export default calculador;
--- src/calculador_cadenas.test.js
+++ src/calculador_cadenas.test.js
@@ -16,7 +16,10 @@ describe("Calcula Cadenas", () => {
   it("deberia retornar 6 al ingresar la cadena 1,2,3 ", () => {
     expect(calculador("1,2,3")).toEqual(6);
   });
-  it("deberia retornar 2 al ingresar la cadena 1-1 ", () => {
-    expect(calculador("1-1")).toEqual(2);
+  it("deberia retornar 3 al ingresar la cadena 1-1,1 ", () => {
+    expect(calculador("1-1,1")).toEqual(3);
+  });
+  it("deberia retornar 17 al ingresar la cadena //[;] 6;7;4 ", () => {
+    expect(calculador("//[;] 6;7;4")).toEqual(17);
   });
 });
