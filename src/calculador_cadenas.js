5ea6e463ed80b7ba93eaa33382b1b08a41a8480b
BMB <bruno.marquez@ucb.edu.bo>
2022-03-27 16:39:12 -0400

    Se ignoran numeros mayores a 1000

--- src/calculador_cadenas.js
+++ src/calculador_cadenas.js
@@ -5,36 +5,39 @@ function calculador(cadena) {
   {
     res = 0;
   }
-  else if(cadena.search(",") != -1 || cadena.search("-") != -1) 
+  else  if(cadena[0] === '/')
+  {
+    res = getSeparator(cadena);
+  } else if(cadena.search(",") != -1 || cadena.search("-") != -1)
   {
     res = splitAdd(cadena, "");
-  } else if(cadena[0 ]=== "/")
-  {
-    res = get_separator(cadena);
   }else res = parseInt(cadena);
 
   return res;
 }
-function get_separator(cadena)
+
+function getSeparator(cadena)
 {
   var cadenaArray;
   cadenaArray = cadena.split(/[[\n ]+/);
-  return splitAdd(cadenaArray[2], cadenaArray[1].slice(0,-1));
+  return splitAdd(cadenaArray[2], cadenaArray[1][0]);
 }
+
 function splitAdd(cadena, separador)
 {
   var res = 0;
-  var cadenaArray;
-  if(separador === "")
+  if(separador != "")
   {
-    cadenaArray = cadena.split(/[,-]+/);
-  }else cadenaArray = cadena.replaceAll(separador, ',').split(/[,-]+/);
-  
-    for(var i = 0; i < cadenaArray.length ; i++)
-    {
-      res = res + parseInt(cadenaArray[i]);
-    }
-    return res;
+    cadena = cadena.replaceAll(separador, ",");
+  }
+  var cadenaArray = cadena.split(/[[,-]+/);
+
+  for(var i = 0; i < cadenaArray.length ; i++)
+  {
+    if( parseInt(cadenaArray[i]) < 1000)
+    res = res + parseInt(cadenaArray[i]);
+  }
+  return res;
 }
 
 export default calculador;
--- src/calculador_cadenas.test.js
+++ src/calculador_cadenas.test.js
@@ -19,7 +19,10 @@ describe("Calcula Cadenas", () => {
   it("deberia retornar 3 al ingresar la cadena 1-1,1 ", () => {
     expect(calculador("1-1,1")).toEqual(3);
   });
-  it("deberia retornar 17 al ingresar la cadena //[;] 6;7;4 ", () => {
-    expect(calculador("//[;] 6;7;4")).toEqual(17);
+  it("deberia retornar 18 al ingresar la cadena //[;] 6;7;4;1 ", () => {
+    expect(calculador("//[;] 6;7;4;1")).toEqual(18);
+  });
+  it("deberia retornar 10 al ingresar la cadena //[;] 5;2;1500;3 ", () => {
+    expect(calculador("//[;] 5;2;1500;3")).toEqual(10);
   });
 });
